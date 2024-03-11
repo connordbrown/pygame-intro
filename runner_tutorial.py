3f26ce536cb67777c9533ce671642a3e87ed768c
Connor <connordbrown@gmail.com>
2024-03-11 16:22:46 -0600

    add background surfaces

--- runner_tutorial.py
+++ runner_tutorial.py
@@ -3,19 +3,29 @@ from sys import exit
 
 # initialize pygame
 pygame.init()
-# create display surface (game window)
+# create display surface (width, height)
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 # create clock object to control frame rate
 clock = pygame.time.Clock()
+# create a font
+test_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Black')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit() # more secure than break
-    # draw all elements
-    # update everything
+    # add surfaces to screen
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+
+    # draw all elements, update everything
     pygame.display.update()
     # set frame rate to 60 fps
     clock.tick(60)
