67ec2fc76f32897879eea1a34d56283e07fa07cb
Connor <connordbrown@gmail.com>
2024-03-11 20:21:26 -0600

    animate snail - initial

--- runner_tutorial.py
+++ runner_tutorial.py
@@ -11,9 +11,13 @@ clock = pygame.time.Clock()
 # create a font
 test_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert() # convert to game pixel format - increase performance
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
+
+# snail
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha() # remove background black and white
+snail_x_pos = 800 # initial position
 
 while True:
     for event in pygame.event.get():
@@ -24,6 +28,9 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surface, (snail_x_pos, 250))
+    snail_x_pos -= 4
+    if snail_x_pos < -100 : snail_x_pos = 800
 
     # draw all elements, update everything
     pygame.display.update()
